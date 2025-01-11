4acf034bb121803cc9f608414933dfc98dd0f946
alekex-10 <kevagalv@espol.edu.ec>
2025-01-10 20:40:47 -0500

    sección B

--- src/tallerpruebas/EmployeeType.java
+++ src/tallerpruebas/EmployeeType.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
+
 package tallerpruebas;
 
-public enum EmployeeType {
-	Worker, Supervisor, Manager;
-}
-=======
-package tallerpruebas;
-
-public enum EmployeeType {
-	Worker, Supervisor, Manager;
-}
->>>>>>> 6e2d188c4bb711a177b0e13a36ae33dfa3e82c5c
+public enum EmployeeType{
+	Worker, Supervisor, Manager}
